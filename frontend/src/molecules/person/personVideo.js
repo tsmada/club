dd9cb27c84a3d33b18975d93e16f6f4db6e08792
tsmada <adam@serouslabs.com>
2020-05-29 17:20:45 -0400

    fix: binding video sourceObj to .current returned from React.createRef()

--- frontend/src/molecules/person/personVideo.js
+++ frontend/src/molecules/person/personVideo.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/media-has-caption */
-import React from 'react'
+import React, { useRef } from 'react'
 import { makeStyles } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
@@ -10,12 +10,18 @@ const useStyles = makeStyles(() => ({
 }))
 
 function PersonVideo({ person }) {
-  const classes = useStyles()
+  const classes = useStyles();
+  let videoRef = useRef();
+
+  // Set the srcObject once the react ref returns current
+  if (videoRef.current && !videoRef.current.srcObject) {
+    videoRef.current.srcObject = person.stream;
+  }
 
   return (
     <video
       // eslint-disable-next-line no-param-reassign
-      ref={(video) => { video.srcObject = person.stream }}
+      ref={videoRef}
       autoPlay
       muted={person.muted}
       className={classes.video}
